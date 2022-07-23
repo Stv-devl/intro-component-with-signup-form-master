87d4a5e25891ca536daaf6d550fc31a56e2afe48
Steven-Dev <107848175+Stv-devl@users.noreply.github.com>
2022-07-23 16:25:55 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const inputs = document.querySelectorAll(
 );
 let firstname, lastname, email, password;
 
-//variable errorDisplay for doing only variables for all
+//variable errorDisplay
 const errorDisplay = (tag, message, valid) => {
   const container = document.querySelector("." + tag + "-container");
   const span = document.querySelector("." + tag + "-container > span");
@@ -13,10 +13,18 @@ const errorDisplay = (tag, message, valid) => {
   if (!valid) {
     container.classList.add("error");
     span.textContent = message;
-  } else {
+  }
+};
+
+const sucessDisplay = (tag, message, valid) => {
+  const container = document.querySelector("." + tag + "-container");
+  const span = document.querySelector("." + tag + "-container > span");
+
+  if (firstname && lastname && email && password == valid) {
     container.classList.remove("error");
     span.textContent = message;
   }
+  console.log(sucessDisplay);
 };
 
 //variable for check firstname
@@ -54,8 +62,6 @@ const passwordChecker = (value) => {
     )
   ) {
     password = null;
-  } else if (value.length < 12) {
-    password = value;
   } else {
     password = value;
   }
@@ -86,6 +92,30 @@ inputs.forEach((input) => {
 //If everything is complete send data when submit, if not alert message ask to fill the forms
 form.addEventListener("submit", (e) => {
   e.preventDefault();
+  if (firstname == null) {
+    errorDisplay(
+      "firstname",
+      "The first name must be between 3 and 20 characters"
+    );
+  }
+
+  if (lastname == null) {
+    errorDisplay(
+      "lastname",
+      "The Last name must be between 3 and 20 characters"
+    );
+  }
+
+  if (email == null) {
+    errorDisplay("email", "Email is not valid");
+  }
+
+  if (password == null) {
+    errorDisplay(
+      "password",
+      "Minimum of 8 characters, one capital letter, one number and one special character"
+    );
+  }
   if (firstname && lastname && email && password) {
     const data = {
       firstname,
@@ -94,40 +124,13 @@ form.addEventListener("submit", (e) => {
       password,
     };
     console.log(data);
+    alert("Thank you, we will back to you soon");
     //=>for empty form after complet
     inputs.forEach((input) => (input.value = ""));
-    alert("Thank you, we will back to you soon");
-    firstname = true;
-    lastname = true;
-    email = true;
-    password = true;
-  }
-  if (firstname == null) {
-    errorDisplay(
-      "firstname",
-      "The first name must be between 3 and 20 characters"
-    );
-    firstname = null;
-  }
-
-  if (lastname == null) {
-    errorDisplay(
-      "lastname",
-      "The Last name must be between 3 and 20 characters"
-    );
-    lastname = null;
-  }
-
-  if (email == null) {
-    errorDisplay("email", "Email is not valid");
-    email = null;
-  }
-
-  if (password == null) {
-    errorDisplay(
-      "password",
-      "Minimum of 8 characters, one capital letter, one number and one special character"
-    );
-    email = null;
+    errorDisplay("firstname", "");
+    errorDisplay("lastname", "");
+    errorDisplay("email", "");
+    errorDisplay("password", "");
+    errorDisplay("password", "");
   }
 });
